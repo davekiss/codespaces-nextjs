b19fc8d6867c87ad10c541929f33d5fd5d26b5df
Wasee Malik <102575920+wmalik-mux@users.noreply.github.com>
2023-04-07 01:30:37 +0000

    Split up form and upload steps

--- app/upload/page.js
+++ app/upload/page.js
@@ -7,27 +7,7 @@ import Link from 'next/link';
 export default function Upload() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-
-  useEffect(() => {
-    const muxUploader = document.querySelector("mux-uploader");
-    muxUploader.endpoint = async function () {
-      const response = await fetch('/api/uploads', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title, description }),
-      });
-
-      setTitle('');
-      setDescription('');
-
-      const data = await response.json();
-      return data.url;
-    };
-
-    return () => muxUploader.endpoint = null;
-  }, [title, description])
+  const [uploadUrl, setUploadUrl] = useState('');
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -37,32 +17,56 @@ export default function Upload() {
     setDescription(e.target.value);
   };
 
+  const handleCreateUploadUrlSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/api/uploads', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, description }),
+    });
+
+    const data = await response.json();
+    setUploadUrl(data.url);
+  }
+
   return (
     <div style={{ maxWidth: '400px', margin: '0 auto' }}>
       <Link href="/">Home</Link>
       <h1>Upload a video</h1>
-      <label htmlFor="title" style={{ display: 'block', marginBottom: '5px' }}>
-        Title
-        <input
-          id="title"
-          type="text"
-          value={title}
-          onChange={handleTitleChange}
-          placeholder="Enter video title"
-          style={{ width: '100%', padding: '8px', borderRadius: '5px', border: '1px solid #ccc', marginBottom: '10px' }}
-        />
-      </label>
-      <label htmlFor="description" style={{ display: 'block', marginBottom: '5px' }}>
-        Description
-        <textarea
-          id="description"
-          value={description}
-          onChange={handleDescriptionChange}
-          placeholder="Enter video description"
-          style={{ width: '100%', padding: '8px', borderRadius: '5px', border: '1px solid #ccc', marginBottom: '10px' }}
-        />
-      </label>
-      <MuxUploader />
+      <form onSubmit={handleCreateUploadUrlSubmit}>
+        <label htmlFor="title" style={{ display: 'block', marginBottom: '5px' }}>
+          Title
+          <input
+            id="title"
+            type="text"
+            value={title}
+            onChange={handleTitleChange}
+            placeholder="Enter video title"
+            style={{ width: '100%', padding: '8px', borderRadius: '2px', border: '1px solid #ccc', marginBottom: '10px' }}
+            disabled={!!uploadUrl}
+          />
+        </label>
+        <label htmlFor="description" style={{ display: 'block', marginBottom: '5px' }}>
+          Description
+          <textarea
+            id="description"
+            value={description}
+            onChange={handleDescriptionChange}
+            placeholder="Enter video description"
+            style={{ width: '100%', padding: '8px', borderRadius: '2px', border: '1px solid #ccc', marginBottom: '10px' }}
+            disabled={!!uploadUrl}
+          />
+        </label>
+        <button disabled={!!uploadUrl} type="submit" style={{ padding: '10px 20px' }}>
+          Create Upload URL
+        </button>
+      </form>
+      {!!uploadUrl && <>
+        <hr />
+        <MuxUploader endpoint={uploadUrl} />
+      </>}
     </div>
   );
 }
