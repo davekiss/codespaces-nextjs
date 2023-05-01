5b422a82f15906116ad49a52436249473afa87ef
Wasee Malik <wmalik@mux.com>
2023-05-01 15:17:52 -0400

    Comments

---
+++ app/videos/[playbackId]/Comments.js
@@ -0,0 +1,36 @@
+import { Suspense } from 'react'
+
+async function CommentList({ asset }) {
+  const response = await fetch(
+    `https://reactathon-workshop-functions.vercel.app/api/comments/${asset}`,
+  )
+  const data = await response.json()
+
+  return (
+    <ul>
+      {data.comments.map((comment) => {
+        return (
+          <li key={comment.id}>
+            <p>{comment.content}</p>
+            <p>
+              <i>{comment.updated_at}</i>
+            </p>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+function Comments({ asset }) {
+  return (
+    <div className="comments">
+      <h3>Comments</h3>
+      <Suspense fallback={<p>Loading comments...</p>}>
+        <CommentList asset={asset} />
+      </Suspense>
+    </div>
+  )
+}
+
+export default Comments
